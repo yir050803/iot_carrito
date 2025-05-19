fa06f40461829df2518760e7a49cd65766c98be6
yir050803 <yir050803.act@gmail.com>
2025-05-19 15:02:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.203.231.99/api/devices";
+const API_URL = "http://3.231.162.196/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
